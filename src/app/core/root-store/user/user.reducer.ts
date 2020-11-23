3eaa4cfd68d722a994bd1b1ecef1c1de33d2083f
keithstrickland <keith.strickland@randstadusa.com>
2020-11-23 14:30:01 -0500

    * Progress with ngrx-store

--- src/app/core/root-store/user/user.reducer.ts
+++ src/app/core/root-store/user/user.reducer.ts
@@ -1,19 +1,34 @@
 import {UserAction, UserActionTypes} from '@core/root-store/user/user.action';
 import {User} from '@shared/models/user.model';
 
-const initialState: User = null;
+export interface iFeatureState {
+	data: any;
+	loading: boolean;
+	error: Error | null;
+}
 
-export function UserReducer(state: User = initialState, action: UserAction) {
+export interface iUserState extends iFeatureState {
+	data: User | null;
+}
+
+export const initialState: iUserState = {
+	data: null,
+	loading: false,
+	error: null
+};
+
+export function UserReducer(state: iUserState = initialState, action: UserAction) {
 	switch (action.type) {
-		case UserActionTypes.CREATE_USER:
-			return action.payload;
-		case UserActionTypes.UPDATE_USER:
-			return action.payload;
-		case UserActionTypes.GET_USER:
-			// TODO - Think this will actually be an effect
-		case UserActionTypes.DELETE_USER:
-			return null;
-		default:
-			return state;
+		// handled by UserEffects
+		// case UserActionTypes.LOGIN_USER:
+		// 	console.log('UserReducer, LOGIN_USER action.payload=', action.payload);
+		// 	return {...state, data: action.payload, loading: true};
+		case UserActionTypes.LOGIN_USER_SUCCESS:
+			console.log('UserReducer, LOGIN_USER_SUCCESS action.payload=', action.payload);
+			const user = User.deserialize(action.payload);
+			return {...state, data: user, loading: false};
+		case UserActionTypes.LOGIN_USER_FAILURE:
+			return {...state, data: null, loading: false};
 	}
+	return state;
 }

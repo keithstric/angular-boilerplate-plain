3bb65acebe0a05c0edbe5f5d2aafb7f28badce0b
Keith Strickland <keith@redpillnow.com>
2020-11-29 16:17:26 -0500

    * Fixes styling screw ups for app-card

--- src/app/shared/components/card/card.component.ts
+++ src/app/shared/components/card/card.component.ts
@@ -3,8 +3,7 @@ import {Component, Input, OnInit, ViewEncapsulation} from '@angular/core';
 @Component({
 	selector: 'app-card',
 	templateUrl: './card.component.html',
-	styleUrls: ['./card.component.scss'],
-	encapsulation: ViewEncapsulation.ShadowDom // for styling ng-content
+	styleUrls: ['./card.component.scss']
 })
 export class CardComponent implements OnInit {
 	@Input() withBorder: boolean = false;

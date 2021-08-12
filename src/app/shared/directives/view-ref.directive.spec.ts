7f42ae50d305f190a607a550c9a1edde19bb5980
Keith Strickland <keith.strickland@randstadusa.com>
2021-08-12 11:13:17 -0400

    Fixes issue with not being able to assign properties to the modalComponentContent component * Added modalComponentContentInitialState to support assigning property values to modalComponentContent component * Added the view-ref "appViewRef" directive * Added some other useful features to the confirm-modal component

---
+++ src/app/shared/directives/view-ref.directive.spec.ts
@@ -0,0 +1,8 @@
+import { ViewRefDirective } from './view-ref.directive';
+
+describe('ViewRefDirective', () => {
+  it('should create an instance', () => {
+    const directive = new ViewRefDirective();
+    expect(directive).toBeTruthy();
+  });
+});

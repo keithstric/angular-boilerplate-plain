7b970fa5e284fd84eea203cb1d657571ef4191c5
Keith Strickland <keith@redpillnow.com>
2021-12-09 10:20:45 -0500

    storybook upgrade

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,26 +2,27 @@ const path = require('path');
 
 module.exports = {
   stories: ['../src/**/*.stories.@(ts|js)'],
-  addons: [
-		{
-			name: '@storybook/addon-essentials'
-		},{
-			name: '@storybook/addon-storysource',
-			options: {
-				rule: {
-					include: [path.resolve(__dirname, '../src')]
-				},
-				loaderOptions: {
-					parser: 'typescript',
-					prettierConfig: {
-						printWidth: 100,
-						tabWidth: 2,
-						bracketSpacing: true,
-						singleQuote: true
-					}
-				},
-				enforce: 'pre'
-			}
-		}
-	]
+  addons: [{
+    name: '@storybook/addon-essentials'
+  }, {
+    name: '@storybook/addon-storysource',
+    options: {
+      rule: {
+        include: [path.resolve(__dirname, '../src')]
+      },
+      loaderOptions: {
+        parser: 'typescript',
+        prettierConfig: {
+          printWidth: 100,
+          tabWidth: 2,
+          bracketSpacing: true,
+          singleQuote: true
+        }
+      },
+      enforce: 'pre'
+    }
+  }],
+  core: {
+    builder: "webpack5"
+  }
 };
